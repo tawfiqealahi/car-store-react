da705b78effa15af75868d9dfd7e46358b468f3a
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-17 18:03:19 -0500

    1

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
+import Home from "./components/Home/Home/Home";
+import Login from "./components/Shared/Login/Login";
+import NotFound from "./components/Shared/NotFound/NotFound";
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route  path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
